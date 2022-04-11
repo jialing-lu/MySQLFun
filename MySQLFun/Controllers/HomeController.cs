6ec1ca994adf82c5dfd452315efe4274169735fb
jialing-lu <jialing.lu111@gmail.com>
2022-04-10 23:34:31 -0600

    task2

--- MySQLFun/Controllers/HomeController.cs
+++ MySQLFun/Controllers/HomeController.cs
@@ -1,20 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using MySQLFun.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MySQLFun.Models;
 
 namespace MySQLFun.Controllers
 {
     public class HomeController : Controller
     {
         private IBowlersRepository _repo { get; set; }
-
-        //Constructor
         public HomeController(IBowlersRepository temp)
         {
             _repo = temp;
@@ -22,10 +20,57 @@ namespace MySQLFun.Controllers
 
         public IActionResult Index()
         {
-            var blah = _repo.Bowlers
-                .ToList();
+            var blah = _repo.Bowlers.ToList();
             return View(blah);
         }
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("AddEdit");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Bowler blah)
+        {
+            _repo.AddBowler(blah);
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+
+            var application = _repo.Bowlers.Single(x => x.BowlerID == id);
+
+            return View("AddEdit", application);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Bowler editBowler)
+        {
+            _repo.SaveChanges(editBowler);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var application = _repo.Bowlers.Single(x => x.BowlerID == id);
+
+            return View(application);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Bowler stuff)
+        {
+
+            _repo.DeleteBowler(stuff);
+
+            return RedirectToAction("Index");
+        }
     }
-}    
+}
--- MySQLFun/Models/EFBowlersRepository.cs
+++ MySQLFun/Models/EFBowlersRepository.cs
@@ -8,10 +8,31 @@ namespace MySQLFun.Models
     public class EFBowlersRepository : IBowlersRepository
     {
         private BowlersDbContext _context { get; set; }
-        public EFBowlersRepository (BowlersDbContext temp)
+        public EFBowlersRepository(BowlersDbContext temp)
         {
             _context = temp;
         }
         public IQueryable<Bowler> Bowlers => _context.Bowlers;
+
+        //pasted stuff
+
+        public void SaveChanges(Bowler b)
+        {
+            _context.Update(b);
+            _context.SaveChanges();
+        }
+
+        public void AddBowler(Bowler b)
+        {
+            _context.Add(b);
+            _context.SaveChanges();
+
+        }
+
+        public void DeleteBowler(Bowler b)
+        {
+            _context.Remove(b);
+            _context.SaveChanges();
+        }
     }
 }
